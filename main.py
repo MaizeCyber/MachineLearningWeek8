5c94a8a3aafcfb60f771ecce76e63b0011d98766
Maize Cyber <maizecyber@pm.me>
2025-11-03 21:51:33 -0500

    Added variables

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from Preprocessing import preprocessScale
 from AdaBoost import AdaBoostModel
 from svm import svm_classify
 
+file = "Firewall_Rule_Classification.csv"
+classColumn = "Class"
 
 def maketree():
-    X_train, X_test, y_train, y_test, class_names = preprocessDecisionTree('Obfuscated-MalMem2022.csv', "Class",
-                                                                           "Category")
+    X_train, X_test, y_train, y_test, class_names = preprocessDecisionTree(file=file, classColumn=classColumn)
     d = DecisionTree(tree_depth=4, impurity_metric="gini", X_train=X_train, X_test=X_test, y_train=y_train,
                      y_test=y_test, class_names=class_names)
     d.build_tree()
     d.show_accuracy()
 
 def makelog():
-    X_train, X_test, y_train, y_test, class_names, feature_names = preprocessScale('Obfuscated-MalMem2022.csv', "Class", 0.10,
-                                                                           "Category")
+    X_train, X_test, y_train, y_test, class_names, feature_names = preprocessScale(file=file, classColumn=classColumn)
     print(class_names.tolist())
     L = LogRegression(C = 100, X_train=X_train, X_test=X_test, y_train=y_train,
                      y_test=y_test, class_names=class_names, feature_names=feature_names, max_iter=5000)
@@ -24,9 +24,7 @@ def makelog():
     L.predict_and_test()
 
 def make_svm():
-    X_train, X_test, y_train, y_test, class_names, feature_names = preprocessScale('Obfuscated-MalMem2022.csv',
-                                                                                   "Class", 0.01,
-                                                                                   "Category")
+    X_train, X_test, y_train, y_test, class_names, feature_names = preprocessScale(file=file, classColumn=classColumn, trainingsize=0.10)
 
     SVM = svm_classify(C = 1, X_train=X_train, X_test=X_test, y_train=y_train,
                      y_test=y_test, class_names=class_names, feature_names=feature_names)
@@ -34,8 +32,7 @@ def make_svm():
     SVM.test_and_metrics()
 
 def make_ada():
-    X_train, X_test, y_train, y_test, class_names = preprocessDecisionTree('Obfuscated-MalMem2022.csv', "Class",
-                                                                           "Category")
+    X_train, X_test, y_train, y_test, class_names = preprocessDecisionTree(file=file, classColumn=classColumn)
     d = AdaBoostModel(n_estimators=300, learning_rate=1.0 ,base_tree_depth=4, impurity_metric="gini", X_train=X_train, X_test=X_test, y_train=y_train,
                      y_test=y_test, class_names=class_names)
     d.build_model()
